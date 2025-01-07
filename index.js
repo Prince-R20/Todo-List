20d646d6ae10f9d9f8a4b102b05488c8e67e4a91
Prince-R20 <larrysamuel2020@gmail.com>
2025-01-07 21:23:11 +0100

    Added function to add task

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //To Do List App
+let todoList = [];
+let taskId = 0;
+
+function addTask(title){
+
+    const task = {
+        id: taskId++,
+        title: title,
+        complete: false
+    };
+
+    todoList.push(task);
+    console.log(`Task ${title} added successfully.`)
+}
+
+addTask("test");
+console.log(todoList);
